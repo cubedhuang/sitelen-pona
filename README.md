9d1336c2221d558846ecb154cfed10c262cc71c3
cubedhuang <99danielh@gmail.com>
2023-01-09 02:30:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sitelen pona images
+
+This repository contains images in sitelen pona using the [Fairfax Pona HD](https://www.kreativekorp.com/software/fonts/fairfaxponahd/) font. View any image with the link https://sitelen.nimi.li/img/nimi.png.
---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { Canvas, GlobalFonts } from "@napi-rs/canvas";
+import fs from "fs/promises";
+
+import type { Linku } from "./types.js";
+
+GlobalFonts.loadFontsFromDir("./fonts");
+
+const SIZE = 400;
+
+// ctx.fillText("esun", SIZE / 2, SIZE / 2);
+
+const data: Linku = await fetch("https://linku.la/jasima/data.json").then(res =>
+	res.json()
+);
+
+// const pngData = await canvas.encode("png");
+// await fs.writeFile("test.png", pngData);
+
+const allGlyphs: string[] = [];
+
+for (const word of Object.values(data.data)) {
+	const glyphs = word.sitelen_pona?.split(" ");
+
+	if (!glyphs?.length) continue;
+
+	// Expand glyphs, eg ["misa", "misa5"] -> ["misa", "misa2", "misa3", "misa4", "misa5"]
+	if (glyphs.length > 1) {
+		const lastGlyph = glyphs.pop();
+		const number = Number(lastGlyph!.at(-1));
+
+		for (let i = 2; i <= number; i++) {
+			glyphs.push(`${glyphs[0]}${i}`);
+		}
+	}
+
+	allGlyphs.push(...glyphs);
+}
+
+await Promise.all(
+	allGlyphs.map(async glyph => {
+		const canvas = new Canvas(SIZE, SIZE);
+		const ctx = canvas.getContext("2d");
+
+		ctx.font = `${SIZE}px Fairfax Pona HD`;
+		ctx.textAlign = "center";
+		ctx.textBaseline = "middle";
+
+		ctx.fillStyle = "#ffffff";
+
+		ctx.clearRect(0, 0, SIZE, SIZE);
+		ctx.fillText(glyph, SIZE / 2, SIZE / 2);
+
+		const pngData = await canvas.encode("png");
+		await fs
+			.writeFile(`./img/${glyph}.png`, pngData)
+			.catch(err => console.error(err));
+	})
+);
